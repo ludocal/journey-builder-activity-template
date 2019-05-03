0da41031ad54e450f1c74179272615fa7fb8a288
Ludovic <ludovic@texei.com>
2019-05-03 17:40:17 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,13 @@ exports.index = function(req, res){
         res.render( 'index', {
             title: 'Unauthenticated',
             errorMessage: 'This app may only be loaded via Salesforce Marketing Cloud',
-            configVar: process.env.config,
+            
         });
     } else {
         res.render( 'index', {
             title: 'Journey Builder Activity',
             results: activity.logExecuteData,
+            'configVar': process.env.config,
         });
     }
 };
