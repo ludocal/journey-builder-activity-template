f8154427e4f01572e7e019db70a04b78e933b6fd
Ludovic <ludovic@texei.com>
2019-05-16 18:11:28 +0200

    configVar

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ exports.index = function(req, res){
         res.render( 'index', {
             title: 'Unauthenticated',
             errorMessage: 'This app may only be loaded via Salesforce Marketing Cloud',
-            
+            configVar: 'TEST'
         });
     } else {
         res.render( 'index', {
