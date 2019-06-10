2e40cee312f05d69902a3ad3c9ff856e039ded3d
Ludovic <ludovic@texei.com>
2019-06-10 13:41:23 +0200

    upd

--- routes/activity.js
+++ routes/activity.js
@@ -74,7 +74,7 @@ exports.save = function (req, res) {
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function (req, res) {
-    console.log(console.log(JSON.stringify(req.body)));
+    console.log(JSON.stringify(req.body));
     // example on how to decode JWT
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 
