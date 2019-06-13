cb8446fce2f613f731ad402e7544abf6619a2656
Ludovic <ludovic@texei.com>
2019-06-13 17:46:11 +0200

    up

--- routes/activity.js
+++ routes/activity.js
@@ -1,12 +1,29 @@
 'use strict';
 var util = require('util');
 var https = require('https');
+const ET_Client = require('sfmc-fuelsdk-node');
 
 // Deps
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
+var contextUser = {};
+var clientMC = {};
+
+// const props = ['DEVICE_ID', 'EXT_ID'];
+// const filter = {
+//     leftOperand: 'CustomerKey',
+//     operator: 'equals',
+//     rightOperand: 'BATCH_DEVICE'
+// };
+// client.dataExtensionRow({ props, Name: 'BATCH_DEVICE' }).get((err, response) => {
+//     // code
+//     console.log(JSON.stringify(err));
+//     //console.log(JSON.stringify(response));
+//     console.log(response.body.Results);
+// });
+
 
 exports.logExecuteData = [];
 var configApplication = [{
@@ -14,16 +31,24 @@ var configApplication = [{
     domain: 'mcwd-d2pprjfdcksy88llpp9dv-4',
     jwtSecret: 'jntkXQazwsJZq_dIZsgz3YRGRMs9jpU-AKEhA3BN42WRmWYAT1jcb3aW5J4lzEmidqtHqc6vqY4Z-qEFIOFQv-GtOUDq4e5D-baF4Tk2mSy4aF66zOSX-vH9GpeFjR2r5VasSAYBC42MYui1GJHZ0mLEVkZbaNfT6sZfH2mth58A9-thITgLqgYXFjJ14V3dFtG8uoERp1RKZXeG_l8n9_iWPtG6DaNPFQWVLV-srtiKpZCe68Qw1NSAWBBc3A2',
     mid: 500008428,
-    appAvailable:[
-    {
-        name:'Batch STORE IOS',
-        id: '5CDD1B576095D88F6FE92DA49189D2'
-    },
-    {
-        name: 'BATCH STORE ANDROID',
-        id: '5CDD1B576095D88F6FE92DA49189D2'
-    }]
+    clientId: '75ltplaovygkrhqzrkbi27yj',
+    clientSecret: 'guaLsbOmBEUc6cf11F7npGM3',
+    authOrigin: "https://mcwd-d2pprjfdcksy88llpp9dv-4.auth.marketingcloudapis.com",
+    soapOrigin: "https://mcwd-d2pprjfdcksy88llpp9dv-4.soap.marketingcloudapis.com",
+    appAvailable: [
+        {
+            name: 'Batch STORE IOS',
+            id: '5CDD1B576095D88F6FE92DA49189D2'
+        },
+        {
+            name: 'BATCH STORE ANDROID',
+            id: '5CDD1B576095D88F6FE92DA49189D2'
+        }]
 }];
+
+//contextUser = configApplication[0];
+//initMarketingCloud();
+//logPushEvent();
 
 function logData(req) {
     exports.logExecuteData.push({
@@ -112,13 +137,14 @@ exports.execute = function (req, res) {
                 pushWrapper.group_id = "testCA"; //TODO
                 pushWrapper.recipients = {};
                 pushWrapper.recipients.custom_ids = [decodedArgs.contactIdentifier];
-                if (decodedArgs.formatSelection === "new")
-                {
+                if (decodedArgs.formatSelection === "new") {
                     pushWrapper.message = {};
                     pushWrapper.message.title = decodedArgs.title;
                     pushWrapper.message.body = decodedArgs.body;
-                    sendNewTemplatePush(pushWrapper,(err, msg) => {
-                        
+                    sendNewTemplatePush(pushWrapper, (err, msg) => {
+                        logPushEvent((err, msg) => {
+
+                        });
                     })
                 }
             });
@@ -156,61 +182,75 @@ exports.validate = function (req, res) {
     res.status(200).send('Validate');
 };
 
-function getClientByJWT(reqBody, callback)
-{
-    configApplication.forEach((val, key, configApplication)  => {
+function getClientByJWT(reqBody, callback) {
+    configApplication.forEach((val, key, configApplication) => {
         try {
             var decoded = JWT.verifySync(reqBody, configApplication[key].jwtSecret);
+            contextUser = configApplication[key];
+            initMarketingCloud();
             callback(null, decoded);
         } catch (error) {
-            
+
         }
     });
     callback("ERROR", null);
 }
 
-function sendNewTemplatePush(pushWrapper, callback){
-    // var body = {
-    //     group_id: "testCA",
-    //     recipients: {
-    //         "custom_ids": ["ludovic@texei.com"]
-    //     },
-    //     message: {
-    //         title: "Hello!",
-    //         body: "How's it going?"
-    //     }
-    // };
+function sendNewTemplatePush(pushWrapper, callback) {
     const options = {
         hostname: 'labs.api.batch.com',
         path: '/1.1/5CDD1B576095D88F6FE92DA49189D2/transactional/send',
         method: 'POST',
-        headers : {'Content-Type': "application/json",'X-Authorization': "dcee600f7a7be131481e28ddb40ae1b0"}
-      };
-      var responseString = "";
-      var responseObject;
-      const req = https.request(options, (res) => {
+        headers: { 'Content-Type': "application/json", 'X-Authorization': "dcee600f7a7be131481e28ddb40ae1b0" }
+    };
+    var responseString = "";
+    var responseObject;
+    const req = https.request(options, (res) => {
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
         var str;
         res.on('data', (d) => {
-            responseString +=d;
+            responseString += d;
         });
         res.on('end', (d) => {
-            if (res.statusCode >= 400){
+            if (res.statusCode >= 400) {
                 callback(null, 0);
             }
             console.log(responseString);
-            if (res.statusCode === 200)
-            {             
+            if (res.statusCode === 200) {
                 callback(null, 1);
             }
-          });
-      });
-      
-      req.on('error', (e) => {
+        });
+    });
+
+    req.on('error', (e) => {
         console.error(e);
-      });
-      console.log(JSON.stringify(pushWrapper));
-      req.write(JSON.stringify(pushWrapper));
-      req.end();
+    });
+    console.log(JSON.stringify(pushWrapper));
+    req.write(JSON.stringify(pushWrapper));
+    req.end();
+}
+
+function initMarketingCloud()
+{
+    clientMC = new ET_Client(contextUser.clientId, contextUser.clientSecret, null,
+        {
+            authOrigin: contextUser.authOrigin,
+            soapOrigin: contextUser.soapOrigin,
+            authOptions: {
+                authVersion: 2
+            }
+        });
+}
+function logPushEvent(callback) {
+    const Name = 'BATCH_PUSH';
+    const props = {
+        contact_key: 'ludovic@texei.com',
+        app_key: '111111',
+        token: '1232456756'
+    };
+    clientMC.dataExtensionRow({ Name, props }).post((err, response) => {
+        if (err) throw new Error(err);
+        callback(err, response);
+    });
 }
