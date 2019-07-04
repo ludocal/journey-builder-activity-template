ee9b64cd8973fb6e681deb7c03d53e99eed1ed94
Ludovic <ludovic@texei.com>
2019-07-04 10:53:00 +0200

    up

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -176,6 +176,7 @@ define(['postmonger', 'callout'], function(Postmonger, callout) {
             steps[2].active = false;
             steps[3].active = true;
             connection.trigger('updateSteps', steps);
+            connection.trigger('nextStep');
            
         }
         else if (currentStep.key === 'step2' && formatSelection === 'template'){
