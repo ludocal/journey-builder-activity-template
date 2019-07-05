87cd120503a9411e7f33401c34884c7f4c49a61d
Ludovic <ludovic@texei.com>
2019-07-05 15:40:12 +0200

    remove log

--- routes/activity.js
+++ routes/activity.js
@@ -2,6 +2,13 @@
 var util = require('util');
 var https = require('https');
 const ET_Client = require('sfmc-fuelsdk-node');
+const winston = require('winston');
+const logger = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
 
 // Deps
 const Path = require('path');
@@ -23,52 +30,13 @@ else{
 }
 
 
-function logData(req) {
-    exports.logExecuteData.push({
-        body: req.body,
-        headers: req.headers,
-        trailers: req.trailers,
-        method: req.method,
-        url: req.url,
-        params: req.params,
-        query: req.query,
-        route: req.route,
-        cookies: req.cookies,
-        ip: req.ip,
-        path: req.path,
-        host: req.host,
-        fresh: req.fresh,
-        stale: req.stale,
-        protocol: req.protocol,
-        secure: req.secure,
-        originalUrl: req.originalUrl
-    });
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.host);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
-}
-
 /*
  * POST Handler for / route of Activity (this is the edit route).
  */
 exports.edit = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     //res.send(200, 'Edit');
     res.status(200).send('body');
 };
@@ -79,7 +47,7 @@ exports.edit = function (req, res) {
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     //res.send(200, 'Save');
     res.status(200).send('Save');
 };
@@ -154,7 +122,7 @@ exports.execute = function (req, res) {
 exports.publish = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     //res.send(200, 'Publish');
     res.status(200).send('Publish');
 };
@@ -165,7 +133,7 @@ exports.publish = function (req, res) {
 exports.validate = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     //res.send(200, 'Validate');
     res.status(200).send('Validate');
 };
