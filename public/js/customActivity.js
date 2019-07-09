9d733f67c2ddd1549d1718e2f2792cb5f9e1101a
Ludovic <ludovic@texei.com>
2019-07-09 17:42:57 +0200

    up

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -323,7 +323,6 @@ define(['postmonger', 'callout'], function (Postmonger, callout) {
                 $('#step1').show();
                 connection.trigger('updateButton', {button: 'next', enabled: true});
                 connection.trigger('updateButton', {button: 'back', visible: false});
-                validateStep1();
                 break;
             case 'step2':
                 $('#step2').show();
