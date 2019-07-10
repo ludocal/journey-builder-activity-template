2e89c428719776f2030df9517d82005da09c41d5
Ludovic <ludovic@texei.com>
2019-07-10 11:45:23 +0200

    up

--- routes/index.js
+++ routes/index.js
@@ -18,36 +18,10 @@ const logger = winston.createLogger({
     ]
 });
 
-const fs = require('fs');
-const Path = require('path');
+const envUtility = require(Path.join(__dirname, '..', 'lib', 'envUtility.js'));
+var configApplication = envUtility.getEnv();
 
-var configApplicationRaw, configApplication = [];
 
-// try to get configuration from CUSTOM_ACTIVITY_CONFIGURATION env var
-if (typeof (process.env.CUSTOM_ACTIVITY_CONFIGURATION) !== 'undefined') {
-    configApplicationRaw = process.env.CUSTOM_ACTIVITY_CONFIGURATION;
-    logger.info('Get application configuration from `CUSTOM_ACTIVITY_CONFIGURATION` environment variable');
-}
-// try to get configuration from config/config.json file instead
-else {
-    let configApplicationFilename = 'config/config.json';
-    logger.info('Get application configuration from ' + configApplicationFilename + ' file');
-    try {
-        configApplicationRaw = fs.readFileSync(Path.join(Path.dirname(__dirname), configApplicationFilename));
-    }
-    catch (e) {
-        logger.error('Failed to get or open ' + configApplicationFilename + ' file');
-        process.exit(1);
-    }
-}
-
-try {
-    configApplication = JSON.parse(configApplicationRaw);
-}
-catch(e) {
-    logger.error('Failed to parse application configuration JSON');
-    process.exit(1);
-}
 
 /*
  * GET home page.
