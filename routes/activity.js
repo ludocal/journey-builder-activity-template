ae06a341ef7e744360f937f776fb77cdb21b5a71
Arthur <arthur@batch.com>
2019-07-10 17:34:43 +0200

    deleting useless redondant slugify function

--- routes/activity.js
+++ routes/activity.js
@@ -56,7 +56,7 @@ exports.execute = function (req, res) {
             decodedArgs.appSelection.forEach(element => {
                 //set common values
                 var pushWrapper = {};
-                pushWrapper.group_id = string_to_slug(decodedArgs.groupid) || decoded.activityId;
+                pushWrapper.group_id = decodedArgs.groupid || decoded.activityId;
                 pushWrapper.recipients = {};
                 pushWrapper.recipients.custom_ids = [decodedArgs.contactIdentifier];
                 //set values with template selection
@@ -204,23 +204,4 @@ function logPushEvent(props, callback) {
         logger.debug('INSERTED BATCH_PUSH ROW');
         callback(err, response);
     });
-}
-
-function string_to_slug(str) {
-    str = str.replace(/^\s+|\s+$/g, ''); // trim
-    str = str.toLowerCase();
-
-    // remove accents, swap ñ for n, etc
-    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-    var to = "aaaaeeeeiiiioooouuuunc------";
-    for (var i = 0, l = from.length; i < l; i++) {
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-    }
-
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-        .replace(/\s+/g, '-') // collapse whitespace and replace by -
-        .replace(/-+/g, '-') // collapse dashes
-        .replace(/-+$/, ""); //removing trailing dashes
-
-    return str;
 }
