62b45cfc26147cf96b810219b4cb659ba98474f4
Ludovic <ludovic@texei.com>
2019-07-10 12:20:53 +0200

    up

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var errorhandler = require('errorhandler');
-var https        = require('https');
+var http        = require('http');
 var path        = require('path');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
@@ -40,6 +40,6 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
-https.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
